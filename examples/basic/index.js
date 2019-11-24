97764a8cac7ddee1fc177501e8634a51a61e3ea9
straker <stevenlambert503@gmail.com>
2019-11-23 20:48:33 -0700

    revert examples

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,7 +1,3 @@
 var livingcss = require('../../index');
 
-livingcss('buttons.css', {
-  preprocess: function(context) {
-      console.log(JSON.stringify(context, null, 2));
-  }
-});
+livingcss('buttons.css');
